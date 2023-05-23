43206731b7419dbc802bf11da405c30d87a27101
Eliud Gateri <egateri@gmail.com>
2023-05-23 20:08:06 +0300

    Add login route:

---
+++ routes/login.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const router = express.Router();
+
+var isGetMethod; //Alert EJS if the Method is POST or GET - To use SAME EJS file for both POST and Get Requests
+
+router.get("/login", (req, res) => {
+  
+    // var isGetMethod = true;
+
+    console.log(res);
+
+    res.render("login", { title: "Login Page", isGetMethod:true });
+ 
+    res.end();
+  
+});
+
+router.post("/login", (req, res) => {
+
+  // var isGetMethod = false;
+ var name = req.body.name;
+ var password = req.body.password;
+ var details = {
+      name: name,
+      password: password
+    };
+
+
+  res.render("login", { title: "Login Page", isGetMethod:false, data: details });
+  res.end();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const registerRouter = require("./routes/register");
+const loginRouter = require("./routes/login");
 app.set("view engine", "ejs");
 app.set(dotevn);
 app.use(express.urlencoded({ extended: false }));
@@ -14,6 +15,7 @@ app.use(express.static("public"));
 app.use(indexRouter);
 app.use(usersRouter);
 app.use(registerRouter);
+app.use(loginRouter);
 const PORT = process.env.PORT;
 // Server listen
 app.listen(PORT, () => console.log(`Server listening to port ${PORT}`));
