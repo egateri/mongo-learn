1fcb27fd0ee9ee5eea72b2fb1dfa88b5077947b3
Eliud Gateri <egateri@gmail.com>
2023-05-24 11:22:46 +0300

    Environment

--- db/db-config.js
+++ db/db-config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true, family: 4})
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
     
